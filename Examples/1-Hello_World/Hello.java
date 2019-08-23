fa2a077df8cff5fdd6a787b8314b93c55dce91db
Dan S. Myers <dmyers@rollins.edu>
2019-08-23 16:12:45 -0400

    Create Hello.java

---
+++ Examples/1-Hello_World/Hello.java
@@ -0,0 +1,30 @@
+// This is the first program. It prints "Hello, World!" to the screen.
+// CMS 167
+
+// Use comments to add information to your programs.
+
+// A Java program is called a class.
+// The class name must match the file name and should be capitalized.
+// The body of the class is enclosed in curly braces, { and }
+
+public class Hello {
+
+  // Indent the body of the class to show its structure.
+  // I recommend using two spaces of indentation for each pair of curly braces.
+  
+  // Every program has a method named main.
+  // main is the "entry point" for the program.
+  // When your class runs, Java executes the statements within main in the order they appear.
+  
+  public static void main(String[] args) {
+    
+    // Again, indent two spaces within main to show the structure of the code
+    
+    // Use System.out.println to write to the screen
+    // Text strings are enclosed in double quotes
+    // Every Java statement ends with a semicolon
+   
+    System.out.println("Hello, World!");
+  }
+
+}
