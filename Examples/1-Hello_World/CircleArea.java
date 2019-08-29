39ac83207009d35f83c331927812f45218667fe5
Dan S. Myers <dmyers@rollins.edu>
2019-08-29 09:47:23 -0400

    Create CircleArea.java

---
+++ Examples/1-Hello_World/CircleArea.java
@@ -0,0 +1,13 @@
+// Calculate the area of a circle
+// Demonstrates calculations, printing results, and Math
+
+// Math.PI is the built-in constant that is the closest value to true pi
+// Math.pow(a, b) calculates a raised to the power b
+
+public class CircleArea {
+
+  public static void main(String[] args) {
+    System.out.println("Area of a circle with radius 5.0 = " + Math.PI * Math.pow(5.0, 2));
+  }
+  
+}
