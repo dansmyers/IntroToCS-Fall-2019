1539d3ba6e951e3db4ccdbf46af5f9bb9e2c65c2
Dan S. Myers <dmyers@rollins.edu>
2019-09-05 20:23:54 -0400

    Create RectangleArea.java

---
+++ Examples/2-Variables/RectangleArea.java
@@ -0,0 +1,44 @@
+// A simple program illustrating variables in Java
+// CMS 167
+
+// Variables let you give names to data in your programs
+//
+// This is one of the most important programming features, because it
+// makes it possible to write large complex programs.
+//
+// Good use of variables makes your programs easier to read and understand.
+
+public class RectangleArea {
+
+  public static void main(String[] args) {
+  
+    // There are two steps in the life of a variable:
+    // 1. Declaration
+    // 2. Assignment
+    
+    // To declare a variable, specify its type and a name
+    // int is the basic type for an integer variable
+    
+    // These lines create two variables named length and width
+    // Each variable holds a single integer value
+    
+    int length;
+    int width;
+    
+    // Use = to assign a value to a variable
+    // The variable name is always on the left and the new value is on the right
+    // Assignment *replaces* the old variable value with the new one
+    length = 25;
+    width = 10;
+    
+    // Declaration and assignment can be combined into one statement
+    int area = length * width;
+    
+    // Using a variable in a print statement will print its value
+    System.out.println("Length: " + length);
+    System.out.println("Width: " + width);
+    System.out.println("Area: " + area);
+  
+  }
+
+}
