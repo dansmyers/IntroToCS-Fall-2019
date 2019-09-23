40d45b1b78dd5b8c3a75e1b8d746c270066e3fc1
Dan S. Myers <dmyers@rollins.edu>
2019-09-23 12:17:07 -0400

    Create EvenOrOdd.java

---
+++ Examples/5-Methods/EvenOrOdd.java
@@ -0,0 +1,20 @@
+// Test if a value is even or odd using a method
+// CMS 167
+
+public class EvenOrOdd {
+
+    public static boolean isEven(int value) {
+        if (value % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isEven(5));
+        System.out.println(isEven(12);
+        System.out.println(isEven(21));
+    }
+
+}
