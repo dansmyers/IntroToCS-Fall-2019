8347d4876f9b00bd9851ce3010cfa0af160958ae
Dan S. Myers <dmyers@rollins.edu>
2019-09-23 10:12:10 -0400

    Create MultipleMethods.java

---
+++ Examples/5-Methods/MultipleMethods.java
@@ -0,0 +1,26 @@
+// An example of calling multiple methods
+// CMS 167
+
+// What is the output of this program?
+
+public class Multiple Methods {
+
+    public static void methodOne() {
+        System.out.println("Spam.");
+    }
+    
+    
+    public static void methodTwo() {
+        System.out.println("Eggs");
+    }
+    
+    
+    public static void main(String[] args) {
+        methodOne();
+        
+        methodTwo();
+        
+        methodOne();
+    }
+
+}
