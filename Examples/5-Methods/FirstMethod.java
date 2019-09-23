ce7fcce69a975b3242ceb1e15d920be221766a07
Dan S. Myers <dmyers@rollins.edu>
2019-09-23 10:02:57 -0400

    Create FirstMethod.java

---
+++ Examples/5-Methods/FirstMethod.java
@@ -0,0 +1,16 @@
+// Use a method to print a message
+// CMS 167
+
+public class FirstMethod {
+    
+    public static void printMessage() {
+        System.out.println("Hello, there! I'm printing this from within a method.");
+        System.out.println("Every you time you call printMessage, it prints these two lines.");
+    }
+    
+    
+    public static void main(String[] args) {     
+        printMessage();     
+    }
+
+}
