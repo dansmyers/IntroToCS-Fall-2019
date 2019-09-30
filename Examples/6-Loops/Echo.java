239aaf3a7cd1f8abc621599502a77a512b2ba3ce
Dan S. Myers <dmyers@rollins.edu>
2019-09-30 12:47:46 -0400

    Create Echo.java

---
+++ Examples/6-Loops/Echo.java
@@ -0,0 +1,26 @@
+/**
+ * Echo using a loop until the user types "stop"
+ *
+ * @author CMS 167
+ */
+ 
+ import java.util.Scanner;
+ 
+public class Echo {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        boolean echoing = true;
+        
+        while(echoing) {
+            String line = input.nextLine();
+            System.out.println(line);
+            
+            if (line.equals("stop")) {
+                echoing = false;
+            }
+        }
+    }
+    
+}
