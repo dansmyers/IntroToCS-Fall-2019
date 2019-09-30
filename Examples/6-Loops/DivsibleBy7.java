3510b88648ea539c9a1ac92307d3b208485f452f
Dan S. Myers <dmyers@rollins.edu>
2019-09-30 12:45:34 -0400

    Create DivsibleBy7.java

---
+++ Examples/6-Loops/DivsibleBy7.java
@@ -0,0 +1,22 @@
+/**
+ * Print the values less than 100 divisible by 7
+ *
+ * @author CMS 167
+ */
+ 
+public class DivisibleBy7 {
+
+    /**
+     * Main
+     */
+    public static void main(String[] args) {
+        int n = 1;
+        
+        while (n < 100) {
+            if (n % 7 == 0) {
+                System.out.println(n);
+            }
+        }
+    }
+    
+}
