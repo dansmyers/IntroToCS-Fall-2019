ae5afbdc1db09c6eb789d507668ea996c42251fc
Dan S. Myers <dmyers@rollins.edu>
2019-09-30 12:38:04 -0400

    Update Countdown.java

--- Examples/6-Loops/Countdown.java
+++ Examples/6-Loops/Countdown.java
@@ -4,4 +4,19 @@
  * @author CMS 167
  */
  
+public class Countdown {
  
+    public static void main(String[] args) {
+     
+        int n = 5;
+      
+        while (n > 0) {
+            System.out.println(n);
+            n = n - 1;
+        }
+     
+        System.out.println("Blastoff!");
+     
+    }
+ 
+}
